2ae1d0fd505e72ec0dc740a66b8e809455310873
jpomfret <jpomfret7@gmail.com>
2025-04-14 07:07:36 +0000

    enable cookie banner

---
+++ blog/static/js/ga.js
@@ -0,0 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-YL15E7BXXF', {
+  'anonymize_ip': true,
+  'allow_ad_personalization_signals': false
+});
